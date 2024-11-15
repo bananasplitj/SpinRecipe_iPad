9271538961d054d8c44d9991e534f974852426b0
bananasplitj <kimj1152@newschool.edu>
2024-11-15 02:42:24 -0500

    Add files via upload

---
+++ iPad_script.js
@@ -0,0 +1,12 @@
+function toggleBold(element) {
+  element.classList.toggle("bold");
+}
+function showPopup() {
+  const popup = document.getElementById("popup");
+  popup.style.display = "flex";
+}
+
+function closePopup() {
+  const popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
